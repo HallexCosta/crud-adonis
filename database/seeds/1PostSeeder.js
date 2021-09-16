434ceaa7426164c3efd28646baf10665edc844cb
HallexCosta <hallex.costa@hotmail.com>
2021-09-16 18:04:19 -0300

    add seeder Post

---
+++ database/seeds/1PostSeeder.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| PostSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use("Factory");
+const Post = use("App/Models/Post");
+
+class PostSeeder {
+  async run() {
+    await Factory.model("App/Models/Post").createMany(2);
+
+    const posts = (await Post.all()).toJSON();
+
+    console.log({
+      posts,
+    });
+  }
+}
+
+module.exports = PostSeeder;
