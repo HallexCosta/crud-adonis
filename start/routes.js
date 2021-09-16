9f3b4496505c827984cc3ac565ed9a42c691fbd0
HallexCosta <hallex.costa@hotmail.com>
2021-09-16 15:13:16 -0300

    add method update and destroy on routes

--- start/routes.js
+++ start/routes.js
@@ -20,9 +20,5 @@ const Route = use("Route");
 Route.get("/users", "UserController.index");
 Route.get("/users/:id", "UserController.show");
 Route.post("/users", "UserController.store");
-Route.put("/users/:id", () => {
-  console.log("> PUT /users/:id");
-});
-Route.delete("/users/:id", () => {
-  console.log("> DELETE /users/:id");
-});
+Route.put("/users/:id", "UserController.update");
+Route.delete("/users/:id", "UserController.destroy");
