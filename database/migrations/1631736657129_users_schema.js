8d0c50ab3b4e59bec4d0fa1baff97406708b255c
HallexCosta <hallex.costa@hotmail.com>
2021-09-16 18:03:16 -0300

    add username, email and password column

--- database/migrations/1631736657129_users_schema.js
+++ database/migrations/1631736657129_users_schema.js
@@ -1,19 +1,22 @@
-'use strict'
+"use strict";
 
 /** @type {import('@adonisjs/lucid/src/Schema')} */
-const Schema = use('Schema')
+const Schema = use("Schema");
 
 class UsersSchema extends Schema {
-  up () {
-    this.create('users', (table) => {
-      table.increments()
-      table.timestamps()
-    })
+  up() {
+    this.create("users", (table) => {
+      table.increments();
+      table.string("username", 80).notNullable().unique();
+      table.string("email", 254).notNullable().unique();
+      table.string("password", 60).notNullable();
+      table.timestamps();
+    });
   }
 
-  down () {
-    this.drop('users')
+  down() {
+    this.drop("users");
   }
 }
 
-module.exports = UsersSchema
+module.exports = UsersSchema;
