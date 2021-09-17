f99d14555f8650e4472d173796a5a80e5c5f4d70
HallexCosta <hallex.costa@hotmail.com>
2021-09-17 00:02:19 -0300

    add routes post and profile

--- start/routes.js
+++ start/routes.js
@@ -22,3 +22,15 @@ Route.get("/users/:id", "UserController.show");
 Route.post("/users", "UserController.store");
 Route.put("/users/:id", "UserController.update");
 Route.delete("/users/:id", "UserController.destroy");
+
+Route.get("/profiles", "ProfileController.index");
+Route.get("/profiles/:id", "ProfileController.show");
+Route.post("/profiles", "ProfileController.store");
+Route.put("/profiles/:id", "ProfileController.update");
+Route.delete("/profiles/:id", "ProfileController.destroy");
+
+Route.get("/posts", "PostController.index");
+Route.get("/posts/:id", "PostController.show");
+Route.post("/posts", "PostController.store");
+Route.put("/posts/:id", "PostController.update");
+Route.delete("/posts/:id", "PostController.destroy");
