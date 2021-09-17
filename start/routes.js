f7ed76676f750727353ab1513a76ce9412069bd5
HallexCosta <hallex.costa@hotmail.com>
2021-09-17 11:42:12 -0300

    add endpoint to list users with posts and profiles with users

--- start/routes.js
+++ start/routes.js
@@ -18,12 +18,16 @@ const Route = use("Route");
 
 // Route.on("/").render("welcome");
 Route.get("/users", "UserController.index");
+Route.get("/users/posts", "UserController.indexPosts");
+Route.get("/users/posts/:id", "UserController.showPosts");
 Route.get("/users/:id", "UserController.show");
 Route.post("/users", "UserController.store");
 Route.put("/users/:id", "UserController.update");
 Route.delete("/users/:id", "UserController.destroy");
 
 Route.get("/profiles", "ProfileController.index");
+Route.get("/profiles/users", "ProfileController.indexUser");
+Route.get("/profiles/users/:id", "ProfileController.showUser");
 Route.get("/profiles/:id", "ProfileController.show");
 Route.post("/profiles", "ProfileController.store");
 Route.put("/profiles/:id", "ProfileController.update");
